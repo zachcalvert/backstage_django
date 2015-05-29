473b5f49b378730ade25e1ad3065dff62e24c5c1
Zach Calvert <zachc@darkhorse.com>
2015-05-28 23:04:12 -0700

    service endpoint tests

--- README.md
+++ README.md
@@ -19,16 +19,16 @@ Developed as part of the interview process with Backstage.
 * Develop a Django web service that can be queried at the following endpoint: localhost:8000/difference?number=n , where n is any integer greater than 0
 and less than or equal to 100.
 
-* The service should emit a JSON object of the following structure:
+* Create a backbone application (or any other front end framework or just
+jquery) based on the above backend service that should display a list of
+the above values in the four columns described above.
+
+* The web service should emit a JSON object of the following structure:
 ```
 {
 "datetime":current_datetime,
 "value":solution,
 "number":n,
 "occurrences":occurrences // the number of times n has been requested
-}```
+}
 
-
-* Create a backbone application (or any other front end framework or just
-jquery) based on the above backend service that should display a list of
-the above values in the four columns described above.
--- squares/tests.py
+++ squares/tests.py
@@ -32,9 +32,13 @@ class CalculationTest(TestCase):
 
 class ServiceTest(TestCase):
 	"""
+	Verify the constructed endpoint is available and returns data in the
+	expected format
 	"""
 	def setUp(self):
 		"""
+		The test db seems to be reading the same data as the "live" db, 
+		hence the get_or_creates
         """
         Calculation.objects.get_or_create(number=1, value=1)
         Calculation.objects.get_or_create(number=10, value=2640)
@@ -43,10 +47,29 @@ class ServiceTest(TestCase):
 	def test_service_endpoint(self):
 		response = self.client.get("%s?number=1" % reverse('get_difference'))
 		self.assertEqual(response.status_code, 200)
-		self.assertContains(response.content, "number")
-		self.assertContains(response.content, "value")
-		self.assertContains(response.content, "occurrences")
-		self.assertContains(response.content, "last_requested")
+		self.assertEqual(1, Calculation.objects.get(number=1).occurrences)
+
+		# self.assertContains(response.content, "number")
+		# self.assertContains(response.content, "value"
+		# self.assertContains(response.content, "occurrences"
+		# self.assertContains(response.content, "last_requested"
+
+		response = self.client.get("%s?number=1" % reverse('get_difference'))
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(2, Calculation.objects.get(number=1).occurrences)
+
+	# def test_occurrences_incrementor(self):
+	# 	"""
+	# 	The number of occurrences for a given Calculation instance should 
+	# 	increase by 1 after a successful request to that resource
+	# 	"""
+	# 	count = Calculation.objects.get(number=1, value=1).occurrences
+
+	# 	response = self.client.get("%s?number=1" % reverse('get_difference'))
+	# 	self.assertEqual(response.status_code, 200)
+
+	# 	self.assertEqual(count+1, Calculation.objects.get(number=1).occurrences)
+
 
 
 
