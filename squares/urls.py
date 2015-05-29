c0001d10e540f7086c83ce57a28e441eb3ecfe32
Zach Calvert <zachc@darkhorse.com>
2015-05-28 21:03:31 -0700

    landing page and static files

--- squares/urls.py
+++ squares/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 import views
 
 urlpatterns = [
+	url(r'^$', TemplateView.as_view(template_name="home.html")),
     url(r'^difference/$', views.get_difference, name='get_difference'),
 ]
